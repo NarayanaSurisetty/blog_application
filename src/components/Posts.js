65dd446d712e22a4698ffacefd25ef5b3bc0c6b1
NarayanaSurisetty <156322851+NarayanaSurisetty@users.noreply.github.com>
2024-02-02 18:02:41 +0530

    [Home] Modified Home page

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -5,12 +5,12 @@ import Post from "./Post.js";
 const Posts = (props) => {
     return (
         <div className="posts-container mt-3 mb-3">
-            {props.getAllPostsData ? props.getAllPostsData.map((post) => (
+            {props.getAllPostsData && props.getAllPostsData.length > 0 ? props.getAllPostsData.map((post) => (
                 <div key={post.id}>
                     <Post post={post} />
                     <br />
                 </div>
-            )) : "No Posts available.."}
+            )) : <p style={{textAlign: "center"}}>No Posts available..</p>}
         </div>
     );
 };
